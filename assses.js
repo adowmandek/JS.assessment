52d220eb383d41550b11c995dda51e6a78b301dd
adowmandek <adowmandek@gmail.com>
2021-06-10 20:09:17 +0300

    first commit

---
+++ assses.js
@@ -0,0 +1,203 @@
+
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = ["mandek","AnitaB","Javascript",10];
+    }
+
+
+  
+    function showResults(){
+      // gather answer from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.results');
+    }
+// Variables
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+
+
+
+
+
+
+
+
+
+//question2//
+
+// Global Variables − A global variable has global scope which means it can be defined anywhere in your JavaScript code.
+// Local Variables − A local variable will be visible only within a function where it is defined. Function parameters are always local to that function.
+
+
+    
+    //question3//
+  // How create object in JavaScript//
+
+
+  let person = {firstName:"Mandek", lastName:"Adow", age:20,};
+  console.log(person)
+
+  //it has property(name) and method//
+
+
+
+
+
+
+
+
+
+
+
+
+
+     //question4//
+
+
+// function two2()
+// {
+//   return
+//   {
+//       bar: "hello"
+//   };
+// }
+//     two2()
+
+
+
+// function one1(){
+//   return {
+//       bar: "hello"
+//   };
+// }
+// one1()
+
+
+
+//function 1 and 2 are similar and phrased by creating variable for it as below//
+
+
+var combination = (function(one1,two2) {
+    return {
+            bar: "hello"
+          };
+  
+  }());
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+     
+  
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
